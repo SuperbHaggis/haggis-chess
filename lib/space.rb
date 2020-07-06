324c7dcb0cb6eca770c1e906303bb1b87f19134f
Turner Jacobs <tsjacobs3@gmail.com>
2020-07-06 11:11:29 +0400

    add @image and add to #initialize

--- lib/space.rb
+++ lib/space.rb
@@ -1,9 +1,10 @@
 class Space
-  attr_accessor :coord, :children, :color
+  attr_accessor :coord, :children, :color, :image
 
   def initialize(coord)
     @coord = coord
     @children = []
     @color = nil
+    @image = @color == 'black' ? '■' : '□'
   end
 end
