2b6a90175e1b3d5c8d583c305ca86f7beb8e2c42
Turner Jacobs <tsjacobs3@gmail.com>
2020-07-06 13:50:06 +0400

    remove @children and add @default_image & #update

--- lib/space.rb
+++ lib/space.rb
@@ -1,10 +1,15 @@
 class Space
-  attr_accessor :coord, :children, :color, :image
+  attr_accessor :coord, :color, :image, :default_image, :open
 
   def initialize(coord1, coord2, color)
     @coord = [coord1, coord2]
-    @children = []
     @color = color
-    @image = @color == 'black' ? '■' : '□'
+    @default_image = @color == 'black' ? '■' : '□'
+    @image = @default_image
+    @open = true
+  end
+
+  def update(piece)
+    @image = piece.image
   end
 end
