a91614bc7a45c74eed126e6bbe5d55234d7be144
Turner Jacobs <tsjacobs3@gmail.com>
2020-07-06 15:42:17 +0400

    add king moveset

--- lib/king.rb
+++ lib/king.rb
@@ -2,5 +2,9 @@ class King < Piece
   def initialize(color)
     @color = color
     @image = color == 'white' ? '♔' : '♚'
+    @moveset = [
+      [1, 0], [1, 1], [0, 1], [-1, 1],
+      [-1, 0], [-1, -1], [0, -1], [1, -1]
+    ]
   end
 end
