c5fe61ec2b220feae6d56afa38a08466f3047a70
Turner Jacobs <tsjacobs3@gmail.com>
2020-07-06 15:38:18 +0400

    add pawn moveset

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -2,5 +2,10 @@ class Pawn < Piece
   def initialize(color)
     @color = color
     @image = color == 'white' ? '♙' : '♟'
+    @moveset = if @color == 'white'
+                 [[1, 0], [2, 0], [1, -1], [1, 1], [2, -1], [2, 1]]
+               else
+                 [[-1, 0], [-2, 0], [-1, -1], [-1, 1], [-2, -1], [-2, 1]]
+               end
   end
 end
