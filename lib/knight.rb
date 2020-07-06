9354672e131f8d51064c0de3152b4fb884b25336
Turner Jacobs <tsjacobs3@gmail.com>
2020-07-06 15:25:17 +0400

    add knight moveset

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,5 +2,9 @@ class Knight < Piece
   def initialize(color)
     @color = color
     @image = color == 'white' ? '♘' : '♞'
+    @moveset = [
+      [1, 2], [-1, -2], [2, 1], [-2, -1], 
+      [1, -2], [2, -1], [-1, -2], [-2, -1]
+    ]
   end
 end
