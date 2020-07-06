d1b6a1cc915662f33d05def887ce51adf46b298a
Turner Jacobs <tsjacobs3@gmail.com>
2020-07-06 15:13:42 +0400

    add Piece#move

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,8 +1,12 @@
 class Piece
   attr_accessor :color, :image, :space, :moveset
 
-  def initialize(color)
+  def initialize(color, space)
     @color = color
+    @space = space
   end
 
+  def move(space)
+    @space = space
+  end
 end
