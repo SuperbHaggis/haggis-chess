5987d0d09d135c52520b5bc9a93075e469cb53f8
Turner Jacobs <tsjacobs3@gmail.com>
2020-07-06 12:45:10 +0400

    fix arguments to reflect new Board generation

--- lib/space.rb
+++ lib/space.rb
@@ -1,8 +1,8 @@
 class Space
   attr_accessor :coord, :children, :color, :image
 
-  def initialize(coord, color)
-    @coord = coord
+  def initialize(coord1, coord2, color)
+    @coord = [coord1, coord2]
     @children = []
     @color = color
     @image = @color == 'black' ? '■' : '□'
