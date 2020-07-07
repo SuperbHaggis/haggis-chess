b31432b78e5ac5251ca7b6862b7e27e7cdaef7d3
Turner Jacobs <tsjacobs3@gmail.com>
2020-07-07 11:46:29 +0400

    add game#play_round

--- lib/game.rb
+++ lib/game.rb
@@ -5,4 +5,9 @@ class Game
     @board = Board.new
     @players = [Player.new('black'), Player.new('white')]
   end
+
+  def play_round
+    move = @players[0].take_turn
+    board[move[0]][move[1]].update(move)
+  end
 end
