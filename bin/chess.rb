1cb01ac8c48e590ef6031396ba450c479edcca38
Turner Jacobs <tsjacobs3@gmail.com>
2020-09-04 15:14:41 +0400

    alter welcome message

--- bin/chess.rb
+++ bin/chess.rb
@@ -4,5 +4,8 @@ require 'pry'
 file = File.open('../docs/title.txt')
 puts file.readlines.map(&:chomp)
 gets.chomp
+puts '>> Welcome to Haggis Chess!'
+puts ">> Type 'help' at any time for a list of useful commands."
+puts ''
 game = Game.new
 game.play
